180d6b99769119dabb22ca66a335939155b719c9
Marta Diez Asensio <mdiez-as@car6s5.42barcelona.com>
2023-05-03 14:49:13 +0200

    Functions calloc, bzero and memmove done

---
+++ atoi.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	
+}
---
+++ memmove.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memove(void *dst, const void *src, size_t len)
+{
+	
+}
