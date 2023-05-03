b89ed0b4fcb636bb617532978ec7ac0ad0ccaf59
Marta Diez Asensio <mdiez-as@car6s5.42barcelona.com>
2023-05-03 15:38:46 +0200

    Names changed

--- isascii.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int	ft_isascii(int c)
-{
-	if (c >= 0 && c <= 177)
-		return (1);
-	else
-		return (0);
-}
--- isprint.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int	ft_isprint(int c)
-{
-	if (c <= 31 && c == 127)
-		return (0);
-	else
-		return (1);
-}
