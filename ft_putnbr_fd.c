8c019935a642cd3feddf1a858a1dce5a3f5b1dc5
Marta Diez Asensio <mdiez-as@car9s5.42barcelona.com>
2023-05-04 21:15:40 +0200

    ft_putnbr added

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,8 +1,46 @@
 #include <unistd.h>
 #include <stdio.h>
 
+size_t ft_strlen(char *s)
+{
+    size_t i;
+
+    i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
+}
+
+char    *ft_itoa(int val, char *str, int base)
+{
+    int    dig;
+    int    num;
+
+    dig = 0;
+    num = val;
+    while (num > 0)
+    {
+        num /= base;
+        dig++;
+    }
+	dig--;
+    str = malloc(dig + 1);
+    while (dig >= 0)
+    {
+        str[dig] = (val % base) + '0';
+        val /= base;
+        dig--;
+    }
+    return (str);
+	free(str);
+}
+
 void    ft_putnbr_fd(int n, int fd)
 {
-    //*itoa
-    //write(fd, &c, 1);
+    char *str;
+	int	len;
+
+	len = ft_strlen(ft_itoa(n, str, 10));
+    write(fd, ft_itoa(n, str, 10) , len);
 }
+
