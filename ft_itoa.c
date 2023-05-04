a97b5b590d3964a7c5672a2d57f263c96a88dcaa
Marta Diez Asensio <mdiez-as@car9s5.42barcelona.com>
2023-05-04 20:23:30 +0200

    ft_itoa added

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,6 +1,34 @@
 #include "libft.h"
 
-char	*itoa(int val, char *str)
+char    *ft_itoa(int val, char *str, int base)
 {
-	
+    int    dig;
+    int    num;
+
+    dig = 0;
+    num = val;
+    while (num > 0)
+    {
+        num /= base;
+        dig++;
+    }
+	dig--;
+    str = malloc(dig + 1);
+    while (dig >= 0)
+    {
+        str[dig] = (val % base) + '0';
+        val /= base;
+        dig--;
+    }
+    return (str);
+	free(str);
 }
+
+/*int    main()
+{
+    int val = 5786345;
+    char str[10];
+    int base = 10;
+
+    printf("%s", ft_itoa(val, str, base));
+}*/
