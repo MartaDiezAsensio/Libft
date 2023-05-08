9a06365a57f2077358c4125114645ef0ae88c379
Marta Diez Asensio <mdiez-as@car6s3.42barcelona.com>
2023-05-08 13:42:02 +0200

    Header added in .h and minor corrections

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 20:21:30 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/05/07 20:23:21 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:41:08 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,8 @@ char	*ft_strchr(char *s, int c)
 
 /*int	main()
 {
-	char	s[] = "hello world";
+	char	s[] = "hello gye";
 	int		c = 'o';
 	printf("%s\n", ft_strchr(s, c));
 	printf("%s", strchr(s, c));
-
-	return (0);
 }*/
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 20:21:50 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/05/08 13:11:32 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:41:28 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+static size_t	ft_strlen(const char *s)
 {
 	size_t i;
 
@@ -36,14 +36,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t	dstsize)
 		dst[dstsize + i] = src[i];
 		i++;
 	}
-	dst[dstsize + i] = '\0';
-	return (dstsize + i);
+	dst[dstsize] = '\0';
+	return (dstsize);
 }
 
-int	main()
+/*int	main()
 {
 	char	dest[11] = "a";
     printf("lu", ft_strlcat(dest, "lorem", 15));
     write(1, "\n", 1);
     write(1, dest, 15);
-}
+}*/
