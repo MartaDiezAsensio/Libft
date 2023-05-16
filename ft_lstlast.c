06de5907c1da19679dfc34789f166a00b09ab88a
Marta Diez Asensio <mdiez-as@cbr3s1.42barcelona.com>
2023-05-16 20:23:02 +0200

    New changes

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 19:41:33 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/05/16 19:48:08 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/05/16 20:10:31 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 t_list	*ft_lstlast(t_list *lst)
 {
 	while (lst)
-		if (!(lst->next))
+	{
+		if (!lst->next)
 			return (lst);
 		lst = lst->next;
+	}
 	return (lst);
 }
